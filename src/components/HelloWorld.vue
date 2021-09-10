9a5b675e910bfeae541ba97d5ec8e34a74aadc76
hirokimoto <hiroki.moto.pro@gmail.com>
2021-09-10 19:10:20 +0900

    feat: add vuex example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="row">
+      <div class="col-md-9 pt-5">
+
+       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3">
+          <Product v-for="product in products" :product="product" :key="product.id" />
+       </div>
+
+      </div>
+      <div class="col-md-3 pt-5">
+        <Cart />
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import Product from "./Product.vue"
+import Cart from "./Cart.vue"
+
+import {computed} from 'vue'
+import {useStore} from "vuex"
+
 export default {
   name: 'HelloWorld',
+  components: {
+    Product,
+    Cart
+  },
   props: {
     msg: String
+  },
+  setup(){
+    const store = useStore()
+
+    let products = computed(function () {
+      return store.state.products
+    })
+
+    let cart = computed(function () {
+      return store.state.cart
+    })
+
+    return {
+      products,
+      cart
+    }
   }
 }
 </script>
