320cb14c84d5e0827d4b31f7cdb308b8e1ce5a1f
hirokimoto <hiroki.moto.pro@gmail.com>
2021-09-10 18:57:56 +0900

    feat: add dynamic route sample

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/price">Price</router-link>
+      <router-link to="/price">Price</router-link> |
+      <router-link to="/price/:name">CryptoCurrency</router-link>
   </div>
   <router-view />
 </template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from "vue-router"
 import HelloWorld from "./components/HelloWorld"
 import Page1 from "./views/Page1"
 import Page2 from "./views/Page2"
+import Page3 from "./views/Page3"
 
 const routes = [
   {
@@ -18,6 +19,12 @@ const routes = [
     path: "/price",
     name: "Price",
     component: Page2,
+  },
+  {
+    path: "/price/:name",
+    name: "User",
+    component: Page3,
+    props: true,
   }
 ]
 
